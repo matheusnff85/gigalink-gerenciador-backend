bc93ee736152037c45ae0571028e46efcd0c1430
matheusnff85 <math.marinho154@gmail.com>
2022-09-14 19:18:14 -0300

    feat: implementa services para create, update, delete e get para os produtos

---
+++ src/services/produto.services.js
@@ -0,0 +1,32 @@
+const { Produto } = require('../database/models');
+
+const getAll = async () => {
+  const result = await Produto.findAll();
+  return { code: 200, data: result };
+};
+
+const getOne = async (id) => {
+  const result = await Produto.findOne({ where: { id } });
+  if (!result) return { code: 404, message: 'Not Found' };
+  return { code: 200, data: result };
+};
+
+const update = async (id, updatedInfos) => {
+  await Produto.update(updatedInfos, { where: { id } });
+  const updated = await Produto.findByPk(id);
+  return { code: 200, data: updated };
+};
+
+const create = async (newItem) => {
+  const created = await Produto.create(newItem);
+  return { code: 201, data: created };
+};
+
+const remove = async (id) => {
+  await Produto.destroy({ where: { id } });
+  return { code: 204 };
+};
+
+module.exports = {
+  getAll, getOne, remove, create, update,
+};
