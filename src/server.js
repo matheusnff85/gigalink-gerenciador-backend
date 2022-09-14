4955a6e9700f4c01fcfccc9cad512d2809d7b5b1
matheusnff85 <math.marinho154@gmail.com>
2022-09-14 20:51:18 -0300

    refac: corrige nome da variavel de ambiente da porta da api

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ import app from './app';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const API_PORT = process.env.API_PORT || 3001;
 
 // eslint-disable-next-line no-console
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
+const server = app.listen(API_PORT, () => console.log(
+  `Server is running on PORT: ${API_PORT}`,
 ));
 
 export default server;
