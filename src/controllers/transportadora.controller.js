f21943b8baeba25fa4cdd41f7856181f0ceb3695
matheusnff85 <math.marinho154@gmail.com>
2022-09-14 20:09:19 -0300

    feat: implementa o controller das transportadoras

---
+++ src/controllers/transportadora.controller.js
@@ -0,0 +1,68 @@
+const transportadoraServices = require('../services/transportadora.services');
+
+const getAll = async (_req, res) => {
+  try {
+    const result = await transportadoraServices.getAll();
+    if ('message' in result) {
+      return res.status(result.code).json({ message: result.message });
+    }
+    return res.status(result.code).json(result.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await transportadoraServices.getOne(id);
+    if ('message' in result) {
+      return res.status(result.code).json({ message: result.message });
+    }
+    return res.status(result.code).json(result.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await transportadoraServices.update(id, req.body);
+    if ('message' in result) {
+      return res.status(result.code).json({ message: result.message });
+    }
+    return res.status(result.code).json(result.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const result = await transportadoraServices.create(req.body);
+    if ('message' in result) {
+      return res.status(result.code).json({ message: result.message });
+    }
+    return res.status(result.code).json(result.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await transportadoraServices.remove(id);
+    if ('message' in result) {
+      return res.status(result.code).json({ message: result.message });
+    }
+    return res.status(result.code).end();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAll, getOne, remove, create, update,
+};
