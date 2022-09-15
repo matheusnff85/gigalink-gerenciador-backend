dab2988be41ab1ab2f559137750b9ce23536683e
matheusnff85 <math.marinho154@gmail.com>
2022-09-15 00:58:33 -0300

    fix: corrige validacoes e implementa as mesmas nos seus respectivos services

--- src/validations/email.validations.js
+++ src/validations/email.validations.js
@@ -16,12 +16,14 @@ const emailSchema = Joi.object({
   }),
 });
 
-const validateNew = (emailObj) => {
+const validateNew = async (emailObj) => {
   const schemaValidate = emailSchema.validate(emailObj);
   if ('error' in schemaValidate) {
     const [code, message] = schemaValidate.error.details[0].message.split('|');
     return { code, message };
   }
+  const fornecedorExists = await Fornecedor.findByPk(emailObj.idFornecedor);
+  if (!fornecedorExists) return { code: 404, message: 'Fornecedor not found' };
   return true;
 };
 
@@ -32,9 +34,9 @@ const validateToUpdate = async (id, emailObj) => {
     return { code, message };
   }
   const { idFornecedor } = emailObj;
-  const emailExists = Email.findByPk(id);
+  const emailExists = await Email.findByPk(id);
   if (!emailExists) return { code: 404, message: 'Email not found' };
-  const fornecedorExists = Fornecedor.findByPk(idFornecedor);
+  const fornecedorExists = await Fornecedor.findByPk(idFornecedor);
   if (!fornecedorExists) return { code: 404, message: 'Fornecedor not found' };
   return true;
 };
