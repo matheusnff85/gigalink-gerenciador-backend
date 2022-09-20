e1e9a1fa3d67b913212b84747961010027397e7d
matheusnff85 <math.marinho154@gmail.com>
2022-09-20 09:42:44 -0300

    feat: adiciona o cors ao projeto

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const emailRouter = require('./routes/email.routes');
 const fornecedorRouter = require('./routes/fornecedor.routes');
 const telefoneRouter = require('./routes/telefone.routes');
@@ -10,6 +11,7 @@ const transportadoraRouter = require('./routes/transportadora.routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/emails', emailRouter);
 app.use('/fornecedores', fornecedorRouter);
